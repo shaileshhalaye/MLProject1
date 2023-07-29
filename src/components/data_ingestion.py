1765921767fbdd5a091f884115aa4889832fc4e1
Shailesh Halaye <shailesh.halaye9@gmail.com>
2023-07-29 23:45:08 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,8 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransfoarmation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 
 
